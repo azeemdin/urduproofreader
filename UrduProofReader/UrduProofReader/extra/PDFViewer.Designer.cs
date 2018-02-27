0b5699fe5f208e8edb2f9b701b333097e62f55ee
Muhammad <masalahuddin.c@stc.com.sa>
2018-02-27 08:49:35 +0300

    UPDATED token file

--- UrduProofReader/UrduProofReader/extra/PDFViewer.Designer.cs
+++ UrduProofReader/UrduProofReader/extra/PDFViewer.Designer.cs
@@ -59,6 +59,7 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -72,14 +73,16 @@
             this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Panel2.Controls.Add(this.uiClose);
             this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.splitContainer1.Size = new System.Drawing.Size(826, 467);
-            this.splitContainer1.SplitterDistance = 414;
+            this.splitContainer1.Size = new System.Drawing.Size(1239, 718);
+            this.splitContainer1.SplitterDistance = 636;
+            this.splitContainer1.SplitterWidth = 6;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -93,8 +96,9 @@
             this.splitContainer2.Panel2.Controls.Add(this.radRichTextEditor1);
             this.splitContainer2.Panel2.Controls.Add(this.richTextEditorRibbonBar1);
             this.splitContainer2.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.splitContainer2.Size = new System.Drawing.Size(826, 414);
-            this.splitContainer2.SplitterDistance = 160;
+            this.splitContainer2.Size = new System.Drawing.Size(1239, 636);
+            this.splitContainer2.SplitterDistance = 245;
+            this.splitContainer2.SplitterWidth = 6;
             this.splitContainer2.TabIndex = 1;
             // 
             // uiPDF
@@ -102,19 +106,23 @@
             this.uiPDF.Dock = System.Windows.Forms.DockStyle.Fill;
             this.uiPDF.Enabled = true;
             this.uiPDF.Location = new System.Drawing.Point(0, 0);
+            this.uiPDF.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uiPDF.Name = "uiPDF";
             this.uiPDF.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("uiPDF.OcxState")));
-            this.uiPDF.Size = new System.Drawing.Size(826, 160);
+            this.uiPDF.Size = new System.Drawing.Size(1239, 245);
             this.uiPDF.TabIndex = 0;
             // 
             // radRichTextEditor1
             // 
             this.radRichTextEditor1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(189)))), ((int)(((byte)(232)))));
+            this.radRichTextEditor1.CommentPaneMinWidth = 435D;
             this.radRichTextEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.radRichTextEditor1.Location = new System.Drawing.Point(0, 174);
+            this.radRichTextEditor1.Location = new System.Drawing.Point(0, 270);
+            this.radRichTextEditor1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.radRichTextEditor1.Name = "radRichTextEditor1";
+            this.radRichTextEditor1.ScaleFactor = new System.Drawing.SizeF(1.5F, 1.538462F);
             this.radRichTextEditor1.SelectionFill = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(78)))), ((int)(((byte)(158)))), ((int)(((byte)(255)))));
-            this.radRichTextEditor1.Size = new System.Drawing.Size(826, 76);
+            this.radRichTextEditor1.Size = new System.Drawing.Size(1239, 115);
             this.radRichTextEditor1.TabIndex = 2;
             this.radRichTextEditor1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.radRichTextEditor1_PreviewKeyDown);
             // 
@@ -123,17 +131,19 @@
             this.richTextEditorRibbonBar1.ApplicationMenuStyle = Telerik.WinControls.UI.ApplicationMenuStyle.BackstageView;
             this.richTextEditorRibbonBar1.AssociatedRichTextEditor = this.radRichTextEditor1;
             this.richTextEditorRibbonBar1.BuiltInStylesVersion = Telerik.WinForms.Documents.Model.Styles.BuiltInStylesVersion.Office2013;
+            this.richTextEditorRibbonBar1.EnableKeyMap = false;
             // 
             // 
             // 
             this.richTextEditorRibbonBar1.ExitButton.Text = "Exit";
             this.richTextEditorRibbonBar1.Location = new System.Drawing.Point(0, 0);
+            this.richTextEditorRibbonBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextEditorRibbonBar1.Name = "richTextEditorRibbonBar1";
             // 
             // 
             // 
             this.richTextEditorRibbonBar1.OptionsButton.Text = "Options";
-            this.richTextEditorRibbonBar1.Size = new System.Drawing.Size(826, 174);
+            this.richTextEditorRibbonBar1.Size = new System.Drawing.Size(1239, 270);
             this.richTextEditorRibbonBar1.TabIndex = 3;
             this.richTextEditorRibbonBar1.TabStop = false;
             ((Telerik.WinControls.UI.RichTextEditorRibbonUI.RichTextEditorRibbonTab)(this.richTextEditorRibbonBar1.GetChildAt(0).GetChildAt(4).GetChildAt(0).GetChildAt(0).GetChildAt(4))).Text = "Mailings";
@@ -142,9 +152,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(678, 14);
+            this.button1.Location = new System.Drawing.Point(1017, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.Size = new System.Drawing.Size(204, 35);
             this.button1.TabIndex = 2;
             this.button1.Text = "فائل تبدیل کیجیے";
             this.button1.UseVisualStyleBackColor = true;
@@ -152,9 +163,10 @@
             // 
             // uiClose
             // 
-            this.uiClose.Location = new System.Drawing.Point(12, 14);
+            this.uiClose.Location = new System.Drawing.Point(18, 22);
+            this.uiClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uiClose.Name = "uiClose";
-            this.uiClose.Size = new System.Drawing.Size(90, 23);
+            this.uiClose.Size = new System.Drawing.Size(135, 35);
             this.uiClose.TabIndex = 0;
             this.uiClose.Text = "بند کیجیے";
             this.uiClose.UseVisualStyleBackColor = true;
@@ -162,11 +174,12 @@
             // 
             // PDFViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(826, 467);
+            this.ClientSize = new System.Drawing.Size(1239, 718);
             this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "PDFViewer";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
